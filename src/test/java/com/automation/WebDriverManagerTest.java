3e82856a7c77e9e3b7f56436801d84a5ae9d9a1e
Rafael Matoso <rafael.matoso@outlook.com>
2023-09-20 19:35:16 +0100

    add/update dependencies

---
+++ src/test/java/com/automation/WebDriverManagerTest.java
@@ -0,0 +1,32 @@
+package com.automation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverManagerTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        driver.get("https://www.selenium.dev/");
+        Assertions.assertTrue(driver.getTitle().equals("Selenium"));
+    }
+
+}
